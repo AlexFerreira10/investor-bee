927800b0aeea92d0b43877ff882f8de566384789
AlexFerreira10 <correaferreiraalex92@email.com>
2024-09-16 16:43:13 -0300

    Initial commit

---
+++ src/main/java/com/investor/bee/InvestorBeeApplication.java
@@ -0,0 +1,13 @@
+package com.investor.bee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestorBeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investor/bee/InvestorBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investor.bee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestorBeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
